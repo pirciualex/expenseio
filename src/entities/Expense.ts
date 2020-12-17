bb58d490d4cc2eb3fd26dfd8dae11e91c240cde0
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-12-17 19:14:19 +0200

    Instantiate mikro-orm and add Expense entity

---
+++ src/entities/Expense.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+@Entity()
+export class Expense {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  createdAt = new Date();
+
+  @Property({ onUpdate: () => new Date() })
+  updatedAt = new Date();
+
+  @Property()
+  value!: number;
+
+  //   @ManyToOne() // when you provide correct type hint, ORM will read it for you
+  //   author!: Author;
+
+  //   @ManyToOne(() => Publisher) // or you can specify the entity as class reference or string name
+  //   publisher?: Publisher;
+
+  //   @ManyToMany() // owning side can be simple as this!
+  //   tags = new Collection<BookTag>(this);
+
+  //   constructor(title: string, author: Author) {
+  //     this.title = title;
+  //     this.author = author;
+  //   }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+
+import { __prod__ } from "./constants";
+import { Expense } from "./entities/Expense";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Expense],
+    dbName: "expenseio",
+    user: "owner",
+    password: "postgres",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+
+  const post = orm.em.create(Expense, { value: 13.3 });
+  orm.em.persistAndFlush(post);
+};
+
+main().catch((err) => {
+  console.error(err);
+});
