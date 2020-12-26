829f3f6ed059401b4950e1c2144e69601357e548
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-12-26 23:15:04 +0200

    Add resolvers for mutations on expenses

--- src/resolvers/expense.ts
+++ src/resolvers/expense.ts
@@ -1,11 +1,60 @@
 import { DbContext } from "src/types"
-import { Ctx, Query, Resolver } from "type-graphql"
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql"
 import { Expense } from "../entities/Expense"
 
 @Resolver()
 export class ExpenseResolver {
   @Query(() => [Expense!]!)
-  expense(@Ctx() { em }: DbContext): Promise<Expense[]> {
+  expenses(@Ctx() { em }: DbContext): Promise<Expense[]> {
     return em.find(Expense, {})
+  }
+
+  @Query(() => Expense)
+  async expense(
+    @Arg("id") id: number,
+    @Ctx() { em }: DbContext
+  ): Promise<Expense> {
+    const expense = await em.findOne(Expense, { id })
+    if (!expense) {
+      throw new Error("Expense not found!")
+    }
+    return expense
+  }
+
+  @Mutation(() => Expense)
+  async createExpense(
+    @Arg("value") value: number,
+    @Ctx() { em }: DbContext
+  ): Promise<Expense | null> {
+    const expense = em.create(Expense, { value })
+    await em.persistAndFlush(expense)
+    return expense
+  }
+
+  @Mutation(() => Expense)
+  async updateExpense(
+    @Arg("id") id: number,
+    @Arg("value") value: number,
+    @Ctx() { em }: DbContext
+  ): Promise<Expense> {
+    const expense = await em.findOne(Expense, { id })
+    if (!expense) {
+      throw new Error("Expense not found!")
+    }
+    expense.value = value
+    return expense
+  }
+
+  @Mutation(() => Boolean)
+  async deleteExpense(
+    @Arg("id") id: number,
+    @Ctx() { em }: DbContext
+  ): Promise<boolean> {
+    const expense = await em.findOne(Expense, { id })
+    if (!expense) {
+      throw new Error("Expense not found!")
+    }
+    await em.nativeDelete(Expense, { id })
+    return true
   }
 }
