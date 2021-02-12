79e388727a7353b4eb94966ec10010c7b7d1d8b9
Alex Pirciu <alex.pirciu@zohomail.eu>
2021-02-12 18:59:50 +0200

    Start changing orm

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,3 +2,6 @@ export const __prod__ = process.env.NODE_ENV !== "production"
 export const __port__ = process.env.PORT || 4000
 export const COOKIE_NAME = "qid"
 export const FORGET_PASSWORD_PREFIX = "forget-password:"
+export const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE || "expensio"
+export const POSTGRES_USERNAME = process.env.POSTGRES_USERNAME || "owner"
+export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || "postgres"
